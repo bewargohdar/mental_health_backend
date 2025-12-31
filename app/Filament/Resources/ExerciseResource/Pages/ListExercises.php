b0eaf1a70e8f5c77cc22cdfd1acaa969264a099f
bewar.gohdar <bewar.00429239@gmail.com>
2026-01-01 01:47:02 +0300

    feat: Add CRUD pages for managing exercises and videos

---
+++ app/Filament/Resources/ExerciseResource/Pages/ListExercises.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ExerciseResource\Pages;
+
+use App\Filament\Resources\ExerciseResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListExercises extends ListRecords
+{
+    protected static string $resource = ExerciseResource::class;
+}
---
+++ app/Filament/Resources/ExerciseResource/Pages/ViewExercise.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ExerciseResource\Pages;
+
+use App\Filament\Resources\ExerciseResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewExercise extends ViewRecord
+{
+    protected static string $resource = ExerciseResource::class;
+}
